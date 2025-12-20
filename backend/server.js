496cfd50412760874ae00607380d6737db572172
Fiksian <Fiksian@gmail.com>
2025-12-21 06:05:16 +0700

    Tambah Setting Biar Ngrok Bisa dipakai

--- backend/server.js
+++ backend/server.js
@@ -24,6 +24,7 @@ const allowedOrigins = [
     'http://127.0.0.1:5173',
     'http://localhost:5174',
     'http://127.0.0.1:5174',
+    'https://unsaturable-neoma-sheathy.ngrok-free.dev'
 ];
 
 app.use(cors({
--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+// vite.config.js
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: true,
+    hmr: {
+      clientPort: 443, // Memaksa HMR menggunakan port HTTPS ngrok
+    },
+  },
 })
